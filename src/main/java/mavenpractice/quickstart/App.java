926067d60717ead6d8d0ba193cde4452153f1dc5
laksh <lakshvijay04@gmail.com>
2025-01-15 14:10:19 +0530

    commits

---
+++ src/main/java/mavenpractice/quickstart/App.java
@@ -0,0 +1,10 @@
+package mavenpractice.quickstart;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
